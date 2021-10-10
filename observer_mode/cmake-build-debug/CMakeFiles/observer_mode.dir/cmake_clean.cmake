0580545d704f4b3ee35114f0f21ef3b0bdfa69c6
sdu-rjy <sdu_rjy@163.com>
2021-10-10 19:51:25 +0800

    ADD:增加观察者模式

---
+++ observer_mode/cmake-build-debug/CMakeFiles/observer_mode.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/observer_mode.dir/main.cpp.o"
+  "observer_mode"
+  "observer_mode.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/observer_mode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
